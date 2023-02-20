d4f2435d3de58a8420427dd9e6e50ddb5c920382
Virginija Jan <vjanuskaite@gmail.com>
2023-02-20 16:55:29 +0200

    add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { MyCanvas } from "./components/Canvas"
 import Component from "./components/Component"
 
 function App() {
   return (
     <div className="App">
       <Component />
+      <MyCanvas />
     </div>
   )
 }
