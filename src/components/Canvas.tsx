ab0d22c1ef34cee998ca23fe4597daef42a6d2ba
Virginija Jan <vjanuskaite@gmail.com>
2023-05-15 13:13:19 +0300

    add glsl loader to webpack config

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,32 +1,27 @@
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
-// @ts-ignore
 import testFragmentShader from "src/shaders/test/fragmentShader.glsl"
-// @ts-ignore
 import testVertexShader from "src/shaders/test/vertexShader.glsl"
 
-export const MyCanvas = () => {
-  console.log(testVertexShader)
-  return (
-    <Canvas
-      id="canvas"
-      style={{ background: "#000" }}
-      camera={{
-        position: [0, 0, 10],
-        near: 0.01,
-        far: 100,
-        fov: 45,
-      }}
-    >
-      <mesh>
-        <planeGeometry args={[1, 1]} />
-        <rawShaderMaterial
-          vertexShader={testVertexShader}
-          fragmentShader={testFragmentShader}
-        />
-      </mesh>
-      <ambientLight intensity={2} />
-      <OrbitControls />
-    </Canvas>
-  )
-}
+export const MyCanvas = () => (
+  <Canvas
+    id="canvas"
+    style={{ background: "#000" }}
+    camera={{
+      position: [0, 0, 10],
+      near: 0.01,
+      far: 100,
+      fov: 45,
+    }}
+  >
+    <mesh>
+      <planeGeometry args={[1, 1]} />
+      <rawShaderMaterial
+        vertexShader={testVertexShader}
+        fragmentShader={testFragmentShader}
+      />
+    </mesh>
+    <ambientLight intensity={2} />
+    <OrbitControls />
+  </Canvas>
+)
